eaf6cacd9119fbf63abd0a5ac26d6fe399aa0ed6
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-01-29 01:14:09 -0400

    first commit

---
+++ src/main/java/ca/com/rlsp/shoponline/ShoponlineSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package ca.com.rlsp.shoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoponlineSpringbootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoponlineSpringbootExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/com/rlsp/shoponline/ShoponlineSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.com.rlsp.shoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoponlineSpringbootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
