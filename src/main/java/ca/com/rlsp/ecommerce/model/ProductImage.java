d10a34cd90c23effe8fc762cec00cc516c836671
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-02-07 14:20:13 -0400

    Add model class ProductImage

---
+++ src/main/java/ca/com/rlsp/ecommerce/model/ProductImage.java
@@ -0,0 +1,75 @@
+package ca.com.rlsp.ecommerce.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_image")
+@SequenceGenerator(name = "seq_product_image", sequenceName = "seq_product_image", initialValue = 1 , allocationSize = 1)
+public class ProductImage implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product_image")
+    private Long id;
+
+    @Column(name = "source_image", columnDefinition = "text")
+    private String sourceImage;
+
+    @Column(columnDefinition = "text")
+    private String thumbnail;
+
+    @ManyToOne(targetEntity = Person.class)
+    @JoinColumn(name = "product_id",
+            nullable = false,
+            foreignKey = @ForeignKey(
+                    value = ConstraintMode.CONSTRAINT,
+                    name = "product_fk"))
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductImage that = (ProductImage) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSourceImage() {
+        return sourceImage;
+    }
+
+    public void setSourceImage(String sourceImage) {
+        this.sourceImage = sourceImage;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
