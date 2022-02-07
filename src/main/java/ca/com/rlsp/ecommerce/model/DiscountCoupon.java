0830746697c73e237cafcb3a82a88419e1b6c3ac
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-02-07 13:27:06 -0400

    Add model class DiscountCoupon

---
+++ src/main/java/ca/com/rlsp/ecommerce/model/DiscountCoupon.java
@@ -0,0 +1,81 @@
+package ca.com.rlsp.ecommerce.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "discount_coupon")
+@SequenceGenerator(name = "seq_discount_coupon", sequenceName = "seq_discount_coupon", initialValue = 1 , allocationSize = 1)
+public class DiscountCoupon implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_discount_coupon")
+    private Long id;
+
+    @Column(name = "coupon_description")
+    private String couponDescription;
+
+    private BigDecimal realValueDiscount;
+
+    private BigDecimal percentualValueDiscount;
+
+    @Temporal(TemporalType.DATE)
+    private Date couponValidate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DiscountCoupon that = (DiscountCoupon) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCouponDescription() {
+        return couponDescription;
+    }
+
+    public void setCouponDescription(String couponDescription) {
+        this.couponDescription = couponDescription;
+    }
+
+    public BigDecimal getRealValueDiscount() {
+        return realValueDiscount;
+    }
+
+    public void setRealValueDiscount(BigDecimal realValueDiscount) {
+        this.realValueDiscount = realValueDiscount;
+    }
+
+    public BigDecimal getPercentualValueDiscount() {
+        return percentualValueDiscount;
+    }
+
+    public void setPercentualValueDiscount(BigDecimal percentualValueDiscount) {
+        this.percentualValueDiscount = percentualValueDiscount;
+    }
+
+    public Date getCouponValidate() {
+        return couponValidate;
+    }
+
+    public void setCouponValidate(Date couponValidate) {
+        this.couponValidate = couponValidate;
+    }
+}
