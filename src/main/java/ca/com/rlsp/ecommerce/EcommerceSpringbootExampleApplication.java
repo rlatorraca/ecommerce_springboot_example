9cb9ba930454d1cf1e25edb108f9f3f8b86ed301
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-02-13 11:22:25 -0400

    Add up new set up into EcommerceSpringbootExampleApplication class

--- src/main/java/ca/com/rlsp/ecommerce/EcommerceSpringbootExampleApplication.java
+++ src/main/java/ca/com/rlsp/ecommerce/EcommerceSpringbootExampleApplication.java
@@ -2,8 +2,14 @@ package ca.com.rlsp.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"ca.com.rlsp.ecommerce.repository"}) // Gerencia e aponta para o base de pacotes de repositorios
+@EnableTransactionManagement// Gerencia as transacoes com o BD
 public class EcommerceSpringbootExampleApplication {
 
     public static void main(String[] args) {
